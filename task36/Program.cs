82a2536a11b14af0a9d750401063271d370ba66c
Alena <alenademy7@gmail.com>
2022-12-12 21:24:30 +0500

    Добавила решение задачи 36

---
+++ task36/Program.cs
@@ -0,0 +1,34 @@
+﻿        Console.WriteLine("Введите количество чисел в массиве");
+        int size = Convert.ToInt32(Console.ReadLine());
+        int[] numbers = new int[size];
+        FillArrayRandomNumbers(numbers);
+
+        PrintArray(numbers);
+    int count = 0;
+
+        void FillArrayRandomNumbers(int[] numbers)
+        {
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = new Random().Next(1,100);
+            }
+        }
+        void PrintArray(int[] numbers)
+        {
+            Console.Write("[ ");
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.Write("]");
+            Console.WriteLine();
+        }
+
+        int sum = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (i % 2 == 1)
+                sum += numbers [i];
+            
+        }
+        Console.WriteLine($"Сумма нечетных элементов массива = {sum}");
